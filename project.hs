589245a3867256b2d4e8fb51cb7f84974af0e719
Mikołaj Storoniak <43695539+MikoStoro@users.noreply.github.com>
2022-12-03 21:27:59 +0100

    Add files via upload

--- project.hs
+++ project.hs
@@ -18,13 +18,31 @@ isPrime k =
     else null [x | x<-[2 .. floor(sqrt(fromIntegral k))], k `mod` x == 0]
 
 createIntList :: Integer -> [Integer]
-createIntList n = nub [ mergeInt x | x<- permutations(splitNum n)]
+createIntList n = [ mergeInt x | x<- rotations(splitNum n)]
+
+spinArray :: [Integer] -> [Integer]
+spinArray intArr = drop 1 (intArr ++ [(head intArr)])
+
+rotationHelper :: Int -> [Integer] -> [[Integer]]
+rotationHelper n intArr = 
+    if n == 0 then []
+    else ((rotationHelper (n-1) (spinArray intArr)) ++ [(spinArray intArr)])
+
+rotations :: [Integer] -> [[Integer]]
+rotations intArr = rotationHelper (length intArr) intArr
+
+checkNumber :: Integer -> Bool
+checkNumber n = null[ x | x <- createIntList(n), isPrime x == False] 
+
+
+checkNumberTest :: Integer -> [Integer]
+checkNumberTest n = [ x | x <- createIntList(n), isPrime x] 
 
 projectFunction :: Integer -> [Integer]
-projectFunction n = [x | x<-[2 .. n], isPrime x == True ]
+projectFunction n = [x | x<-[1 .. n], checkNumber x]
 
 
 main :: IO ()
 main = do print ("Enter Pascal's row number")
           n <- readLn
-          print (pascal (n :: Integer))
+          print ((n :: Integer))
